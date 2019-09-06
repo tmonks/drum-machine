acfe13cc81a177625db2d2aa521190f2086fc2f2
Tom Monks <monksy@gmail.com>
2019-09-06 13:54:07 -0400

    Initial setup and layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ReactFCCTest from "react-fcctest";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactFCCTest />
+        <div id="drum-machine">
+          <div id="display">Hello World</div>
+          <div class="drum-pad">Q</div>
+          <div class="drum-pad">W</div>
+          <div class="drum-pad">E</div>
+          <div class="drum-pad">A</div>
+          <div class="drum-pad">S</div>
+          <div class="drum-pad">D</div>
+          <div class="drum-pad">Z</div>
+          <div class="drum-pad">X</div>
+          <div class="drum-pad">C</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
