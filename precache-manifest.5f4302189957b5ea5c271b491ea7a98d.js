a9af57a46c8f1cdaf37f3bbde5b9cdbf2956a78c
U-LCGH\tom.monks <tom.monks@LISDATA.LCGH.LOCAL>
2019-09-13 15:50:29 -0400

    Updates

---
+++ precache-manifest.5f4302189957b5ea5c271b491ea7a98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0384a7da5a05608c334f73de4ef9c94",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "2f1ddb1906a3dc69d492",
+    "url": "/drum-machine/static/css/main.1467409f.chunk.css"
+  },
+  {
+    "revision": "674307b29974e49c44b5",
+    "url": "/drum-machine/static/js/2.07d6bf1c.chunk.js"
+  },
+  {
+    "revision": "2f1ddb1906a3dc69d492",
+    "url": "/drum-machine/static/js/main.ffb6130a.chunk.js"
+  },
+  {
+    "revision": "719e68e444e7a8359626",
+    "url": "/drum-machine/static/js/runtime~main.5f2b2d63.js"
+  }
+]);
