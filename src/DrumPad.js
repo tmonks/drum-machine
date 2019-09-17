278c58fc41ccdd98ddae4e1d9fbd162484fcd1ac
Tom Monks <monksy@gmail.com>
2019-09-17 13:46:30 -0400

    Minor improvements to color layout & work on keyframes animation

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // import sound from "./assets/test.wav";
 import "./DrumPad.css";
 
 export default function DrumPad(props) {
   let audioRef = null;
 
+  const [lit, setLit] = useState(false);
+
   useEffect(() => {
     if (props.hit) {
       audioRef.currentTime = 0;
       audioRef.play();
+      setLit(true);
     }
   }, [props.hit, audioRef]);
 
+  const handleClick = () => {
+    setLit(false);
+    props.mouseDownHandler(props.drumPadID);
+  };
+
   return (
     <div
-      className={props.hit ? "drum-pad hit" : "drum-pad"}
-      onMouseDown={() => props.mouseDownHandler(props.drumPadID)}
+      className={lit ? "drum-pad hit" : "drum-pad"}
+      onMouseDown={() => handleClick()}
       onMouseUp={() => props.mouseUpHandler(props.drumPadID)}
+      onAnimationEnd={() => setLit(false)}
       id={"drumPad" + props.drumPadID}
     >
       {props.letter}
