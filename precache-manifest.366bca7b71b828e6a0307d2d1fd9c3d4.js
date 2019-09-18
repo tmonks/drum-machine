def25d862b6e0d2c17dbe86fe0f065abdeb18b6b
U-LCGH\tom.monks <tom.monks@LISDATA.LCGH.LOCAL>
2019-09-18 12:43:56 -0400

    Updates

--- precache-manifest.366bca7b71b828e6a0307d2d1fd9c3d4.js
+++ precache-manifest.366bca7b71b828e6a0307d2d1fd9c3d4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e8301b7c58814be952c1dd7c1e7359b6",
+    "revision": "6854883e126deb497c58de103336626a",
     "url": "/drum-machine/index.html"
   },
   {
-    "revision": "ea7d86e84f7607f9561b",
-    "url": "/drum-machine/static/css/main.ccc4d458.chunk.css"
+    "revision": "6dbcab685e1b5770c94d",
+    "url": "/drum-machine/static/css/main.13ceaf55.chunk.css"
   },
   {
     "revision": "674307b29974e49c44b5",
     "url": "/drum-machine/static/js/2.07d6bf1c.chunk.js"
   },
   {
-    "revision": "ea7d86e84f7607f9561b",
-    "url": "/drum-machine/static/js/main.5b02cad3.chunk.js"
+    "revision": "6dbcab685e1b5770c94d",
+    "url": "/drum-machine/static/js/main.1952ee16.chunk.js"
   },
   {
     "revision": "719e68e444e7a8359626",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drum-machine/precache-manifest.a95840a05928a03406dd81cc01891364.js"
+  "/drum-machine/precache-manifest.366bca7b71b828e6a0307d2d1fd9c3d4.js"
 );
 
 self.addEventListener('message', (event) => {
